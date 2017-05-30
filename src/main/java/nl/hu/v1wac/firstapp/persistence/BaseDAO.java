f5afcbdbb071bcdbe7223fde7c5ad27ed737a28b
Justin <jwilkes1506@gmail.com>
2017-05-30 14:00:24 +0200

    Procfile added

--- src/main/java/nl/hu/v1wac/firstapp/persistence/BaseDAO.java
+++ src/main/java/nl/hu/v1wac/firstapp/persistence/BaseDAO.java
@@ -1,22 +1,44 @@
 package nl.hu.v1wac.firstapp.persistence;
 
+import java.net.URI;
 import java.sql.Connection;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class BaseDAO {
-	
-	public Connection getConnection() {
-		Connection result = null;	
+	private DataSource connectionPool;
+ 
+	public BaseDAO() {
 		try {
-			InitialContext ic = new InitialContext();
-			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");	
-			result = ds.getConnection();
+			final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
+			if (DATABASE_URL_PROP != null) {
+				URI dbUri = new URI(DATABASE_URL_PROP);
+				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+				BasicDataSource pool = new BasicDataSource();
+				if (dbUri.getUserInfo() != null) {
+					pool.setUsername(dbUri.getUserInfo().split(":")[0]);
+					pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+				}
+				pool.setDriverClassName("org.postgresql.Driver");
+				pool.setUrl(dbUrl);
+				pool.setInitialSize(1);
+
+				connectionPool = pool;
+			} else {
+				InitialContext ic = new InitialContext();
+				connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	protected final Connection getConnection() {
+		try {
+			return connectionPool.getConnection();
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
-		}	
-		return result;
+		}
 	}
-
 }
